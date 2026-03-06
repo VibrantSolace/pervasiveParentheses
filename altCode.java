cb535310bb8e6c0ef89fe3622f00eed69af5f64b
VibrantSolace <85423035+VibrantSolace@users.noreply.github.com>
2026-03-05 22:12:19 -0800

    Implement parentheses evaluation in pervasiveParentheses

---
+++ altCode.java
@@ -0,0 +1,68 @@
+public class pervasiveParentheses {
+    public static String findP(String s) {
+        int index = s.indexOf(")");
+        if (index == -1) { // safety measures
+            if (s.indexOf("(") != -1) {
+                System.out.println("No matching closed parentheses for your open parentheses");
+                return "error";
+            } else
+                return s;
+        }
+        int i = index; // variable to tick down from index to search String
+        String search = ""; // variable to search String one character at a time
+        while (!search.equals("(")) {
+            search = s.substring(i - 1, i);
+            i -= 1;
+            if (i == 0 && !search.equals("(")) {
+                System.out.println("No matching open parentheses for your closed parentheses");
+                return "error";
+            }
+        }
+        int num = Integer.parseInt(s.substring(i + 1, index)) * 2; // doubles what's inside parentheses
+        // * this next section of code will search for the Strings
+        // before and after the parentheses in order to add them together
+        // and replace them back in the String */
+        String beforeP = ""; // placeholder variable because it's flipped
+        String before = ""; // String before parentheses
+        search = "";
+        while (!search.equals("(")) {
+            beforeP += search;
+            if (i == 0) {
+                i--;
+                break;
+            }
+            search = s.substring(i - 1, i);
+            i -= 1;
+        }
+
+        int len = beforeP.length();
+        while (len != 0) { // reverses the beforeP String
+            before += beforeP.charAt(len - 1);
+            len -= 1;
+        }
+
+        String after = ""; // don't need a placeholder for this one
+        search = "";
+        while (!(search.equals("(") || search.equals(")"))) {
+            after += search;
+            index += 1;
+            if (index == s.length()) {
+                break;
+            }
+            search = s.substring(index, index + 1);
+        }
+
+        if (before.equals(""))
+            before = "0";
+        if (after.equals(""))
+            after = "0";
+
+        num += Integer.parseInt(before) + Integer.parseInt(after);
+        s = s.substring(0, i + 1) + String.valueOf(num) + s.substring(index, s.length());
+        return findP(s);
+    }
+
+    public static void main(String[] args) {
+        System.out.print(findP("((10(123))(10))"));
+    }
+}
